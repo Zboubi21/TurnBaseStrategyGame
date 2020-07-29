fa8c8f0e955a0217183fefe7f4a2766d78023d19
JeremyPayreLaptop <jeremy.payre@dualcat.io>
2020-07-29 22:50:41 +0200

    [Feature] Started to add spawn and destroy mountains

--- Assets/Scripts/Characters/CharacterController.cs
+++ Assets/Scripts/Characters/CharacterController.cs
@@ -4,6 +4,8 @@ namespace TBSG.Combat
 {
     public class CharacterController : MonoBehaviour
     {
+        [SerializeField] private GameObject m_MountainPrefab = null;
+
         private Character m_Character; 
 
         private void Awake()
@@ -18,10 +20,44 @@ namespace TBSG.Combat
 
         private void DetectMouse()
         {
+            if (GridManager.Instance.m_HoveredGridTile == null) return;
+
             if (Input.GetMouseButtonDown(0))
-                if (GridManager.Instance.m_HoveredGridTile != null)
-                    if (m_Character.CanMoveToTile(GridManager.Instance.m_HoveredGridTile))
-                        m_Character.MoveToTile(GridManager.Instance.m_HoveredGridTile, OnCharacterReachedTargetPos); 
+                MovePlayer();
+            if (Input.GetMouseButtonDown(1))
+                SpawnMountainOnTile(GridManager.Instance.m_HoveredGridTile);
+            if (Input.GetMouseButtonDown(2))
+                DestroyMountainOnTile(GridManager.Instance.m_HoveredGridTile);
+        }
+
+        private void MovePlayer()
+        {
+            if (m_Character.CanMoveToTile(GridManager.Instance.m_HoveredGridTile))
+                m_Character.MoveToTile(GridManager.Instance.m_HoveredGridTile, OnCharacterReachedTargetPos); 
+        }
+
+        private void SpawnMountainOnTile(GridTile gridTile)
+        {
+            if (CanSpawnMountainOnTile(gridTile))
+            {
+                GridObject mountainGridObject = m_MountainPrefab.GetComponent<GridObject>();
+                GridManager.Instance.InstantiateGridObject(mountainGridObject, gridTile.m_GridPosition);
+            }
+        }
+
+        private bool CanSpawnMountainOnTile(GridTile gridTile)
+        {
+            return GridManager.Instance.GetGridObjectAtPosition(gridTile.m_GridPosition) == null && !gridTile.m_IsTileFlyable;
+        }
+
+        private void DestroyMountainOnTile(GridTile gridTile)
+        {
+            GridObject go = GridManager.Instance.GetGridObjectAtPosition(gridTile.m_GridPosition);
+            if (go == null) return;
+            if (go.TryGetComponent(out Mountain mountain))
+            {
+                GridManager.Instance.EraseGridObjectAtPosition(gridTile.m_GridPosition);
+            }
         }
 
         private void OnCharacterReachedTargetPos()
