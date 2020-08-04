8ed7fca2f31e9c012fae2e7699b8aee756acceed
JeremyPayreLaptop <jeremy.payre@dualcat.io>
2020-08-04 22:15:00 +0200

    [Code] Polish some scripts

--- Assets/Scripts/Spells/SpellManager.cs
+++ Assets/Scripts/Spells/SpellManager.cs
@@ -1,26 +1,28 @@
 ﻿using UnityEngine;
 using GameDevStack.Patterns;
-using TBSG.Combat;
 
-public class SpellManager : SingletonMonoBehaviour<SpellManager>
+namespace TBSG.Combat
 {
-    // [SerializeField] private SpellParameters[] m_Spells = null;
-    [SerializeField] private PlayerSpellParameters[] m_PlayerSpells = null;
-
-    // public SpellParameters GetDataSpellWithSpellEnum(SpellsEnum spellEnum)
-    // {
-    //     if (m_Spells == null || m_Spells.Length == 0) return null;
-    //     for (int i = 0, l = m_Spells.Length; i < l; ++i)
-    //         if (m_Spells[i].m_Spell == spellEnum)
-    //             return m_Spells[i];
-    //     return null;
-    // }
-    public PlayerSpellParameters GetPlayerSpellWithEnum(SpellsEnum spellEnum)
+public class SpellManager : SingletonMonoBehaviour<SpellManager>
     {
-        if (m_PlayerSpells == null || m_PlayerSpells.Length == 0) return null;
-        for (int i = 0, l = m_PlayerSpells.Length; i < l; ++i)
-            if (m_PlayerSpells[i].m_Spell == spellEnum)
-                return m_PlayerSpells[i];
-        return null;
+        // [SerializeField] private SpellParameters[] m_Spells = null;
+        [SerializeField] private PlayerSpellParameters[] m_PlayerSpells = null;
+
+        // public SpellParameters GetDataSpellWithSpellEnum(SpellsEnum spellEnum)
+        // {
+        //     if (m_Spells == null || m_Spells.Length == 0) return null;
+        //     for (int i = 0, l = m_Spells.Length; i < l; ++i)
+        //         if (m_Spells[i].m_Spell == spellEnum)
+        //             return m_Spells[i];
+        //     return null;
+        // }
+        public PlayerSpellParameters GetPlayerSpellWithEnum(SpellsEnum spellEnum)
+        {
+            if (m_PlayerSpells == null || m_PlayerSpells.Length == 0) return null;
+            for (int i = 0, l = m_PlayerSpells.Length; i < l; ++i)
+                if (m_PlayerSpells[i].m_Spell == spellEnum)
+                    return m_PlayerSpells[i];
+            return null;
+        }
     }
 }
