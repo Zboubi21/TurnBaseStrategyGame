ff0a2de490e2942d0e65eb99bd4a84da4b2f9439
JeremyPayreLaptop <jeremy.payre@dualcat.io>
2020-09-20 17:58:18 +0200

    [Feature] Added loading screen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,15 +5,12 @@ namespace TBSG
 {
     public class GameManager : SingletonMonoBehaviour<GameManager>
     {
-        private LoadingScene m_LoadingScene;
-
         protected override void Awake()
         {
             base.Awake();
-            m_LoadingScene = GetComponent<LoadingScene>();
         }
 
-        public void LoadMainMenu() => m_LoadingScene.LoadMainMenu();
-        public void LoadGame() => m_LoadingScene.LoadGame();
+        public void LoadMainMenu() => LoadingSceneManager.Instance.LoadMainMenu();
+        public void LoadGame() => LoadingSceneManager.Instance.LoadGame();
     }
 }
---
+++ Assets/Scripts/LoadingSceneManager.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using GameDevStack.Patterns;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace TBSG
+{
+    public class LoadingSceneManager : SingletonMonoBehaviour<LoadingSceneManager>
+    {
+        private const string LOAD_ANIMATOR_NAME = "Load";
+        private const float MIN_SCENE_LOADING_TIME = 1;
+
+        [SerializeField] private Image m_LoadingBar = null;
+
+        private AsyncOperation m_SceneLoadingOperation;
+        private float m_LoadingProgress = 0;
+
+        private CanvasGroup m_CanvasGroup;
+        private Animator m_Animator;
+
+        private enum SceneIndexes
+        {
+            MainMenu = 0,
+            InGame = 1,
+        }
+
+        protected override void Awake()
+        {
+            base.Awake();
+            m_CanvasGroup = GetComponent<CanvasGroup>();
+            m_Animator = GetComponent<Animator>();
+        }
+
+        public void LoadMainMenu()
+        {
+            StartCoroutine(LoadScene(SceneIndexes.MainMenu));
+        }
+
+        public void LoadGame()
+        {
+            StartCoroutine(LoadScene(SceneIndexes.InGame));
+        }
+
+        private IEnumerator LoadScene(SceneIndexes scene)
+        {
+            SetLoadingProgress(0);
+            m_CanvasGroup.blocksRaycasts = true;
+            m_Animator.SetBool(LOAD_ANIMATOR_NAME, true);
+
+            yield return new WaitForSeconds(0.5f);
+
+            m_SceneLoadingOperation = SceneManager.LoadSceneAsync((int)scene);
+            m_SceneLoadingOperation.allowSceneActivation = false;
+
+            float time = Time.time;
+            while ((Time.time - time) <= MIN_SCENE_LOADING_TIME || m_SceneLoadingOperation.progress < 0.9f)
+            {
+                float fakeProgress = (Time.time - time) / MIN_SCENE_LOADING_TIME;
+                float sceneLoadingProgress = m_SceneLoadingOperation.progress;
+                m_LoadingProgress = Mathf.Min(fakeProgress, sceneLoadingProgress);
+                SetLoadingProgress(m_LoadingProgress);
+                yield return null;
+            }
+            m_SceneLoadingOperation.allowSceneActivation = true;
+
+            yield return new WaitForSeconds(0.5f);
+
+            m_Animator.SetBool(LOAD_ANIMATOR_NAME, false);
+            m_CanvasGroup.blocksRaycasts = false;
+        }
+
+        private void SetLoadingProgress(float progress)
+        {
+            m_LoadingBar.fillAmount = progress;
+        }
+    }
+}
