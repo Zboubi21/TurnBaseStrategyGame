7c1ed5f9d2814498b4d60fa59693919bbc8377df
JeremyPayreLaptop <jeremy.payre@dualcat.io>
2021-01-09 11:12:41 +0100

    [Feature] Added life points on heads up enemies

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using GameDevStack.Patterns;
+
+namespace TBSG.Combat
+{
+    public class CameraManager : SingletonMonoBehaviour<CameraManager>
+    {
+        [SerializeField] private Camera m_MainCamera = null;
+
+        public Camera GetCurrentCamera()
+        {
+            return m_MainCamera;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Combat/FloatingText/EnemyHeadUpData.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using TMPro;
+
+namespace TBSG.Combat
+{
+    public class EnemyHeadUpData : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private Character m_Character = null;
+        [SerializeField] private TextMeshProUGUI m_LifePointsTxt = null;
+
+        private void Start()
+        {
+            m_Character.OnLifePointsChanged += Character_OnLifePointsChanged;
+
+            Initialize();
+        }
+
+        private void LateUpdate()
+        {
+            transform.rotation = CameraManager.Instance.GetCurrentCamera().transform.rotation;
+        }
+
+        private void Initialize()
+        {
+            Character_OnLifePointsChanged();
+        }
+
+        private void Character_OnLifePointsChanged()
+        {
+            m_LifePointsTxt.text = m_Character.CurrentLifePoints.ToString();
+        }
+
+        private void OnDestroy()
+        {
+            m_Character.OnLifePointsChanged -= Character_OnLifePointsChanged;
+        }
+    }
+}
